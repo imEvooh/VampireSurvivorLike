ed5cb5662b5eb574d06d423b9e1dd92221f8df36
Thomas Durand <thomas.durand@epitech.eu>
2024-02-19 18:53:24 +0100

    [ADD] Autofire + monsters have health and can die with animation

--- Bullet.gd
+++ Bullet.gd
@@ -11,3 +11,9 @@ func _physics_process(delta):
 	travelled_distance += speed * delta;
 	if (travelled_distance > maxRange):
 		queue_free();
+
+
+func _on_body_entered(body):
+	queue_free();
+	if (body.has_method("take_damage")):
+		body.take_damage(5);
