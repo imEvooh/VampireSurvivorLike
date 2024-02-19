01885bed985015f0b2cc1e31b362f01684313bca
Thomas Durand <thomas.durand@epitech.eu>
2024-02-19 17:01:12 +0100

    [ADD] bullet template

---
+++ Bullet.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+var travelled_distance = 0;
+const speed = 1000;
+const maxRange = 1200;
+
+func _physics_process(delta):
+	var direction = Vector2.RIGHT.rotated(rotation);
+	
+	position += direction * speed * delta;
+	travelled_distance += speed * delta;
+	if (travelled_distance > maxRange):
+		queue_free();
